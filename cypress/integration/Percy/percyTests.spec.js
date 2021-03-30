cdaaaf6fa1c6079b3990a78c65aedded2578eeca
robbrowningone@gmail.com <robbrowningone@gmail.com>
2021-03-30 08:10:46 +0100

    added new regression yml file

--- cypress/integration/Percy/percyTests.spec.js
+++ cypress/integration/Percy/percyTests.spec.js
@@ -1,7 +1,15 @@
+import { cookieBanner, homepage, navigation } from '../../pageObjects/objects';
+const cookie = new cookieBanner();
+const home = new homepage();
+
 describe('percy tests', () => {
   it('should screenshot homepage', () => {
     cy.visit('/');
-    cy.wait(3000);
+    cy.get(home.pickswiseLogo).should('be.visible');
+    cy.get(cookie.agreeButton).should('be.visible').click();
+    cy.scrollTo('bottom');
+
+    cy.wait(9000);
     cy.percySnapshot();
   });
 });
