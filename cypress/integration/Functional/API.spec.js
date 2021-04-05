34ea95c74dd3902f8e5866a193a204a4d70bdd94
robbrowningone@gmail.com <robbrowningone@gmail.com>
2021-04-05 20:11:44 +0100

    mocha awesome report example

--- cypress/integration/Functional/API.spec.js
+++ cypress/integration/Functional/API.spec.js
@@ -5,6 +5,6 @@ const nav = new navigation();
 
 describe('API tests', () => {
   it('should test an API', () => {
-    cy.intercept('https://sdk.fra-01.braze.eu/api/v3/data/', {consentedToAny: true})
+    cy.visit('/');
   });
 });
